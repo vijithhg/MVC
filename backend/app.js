eb42903ac0dd205018d52eafdab407eede11e8a1
vijithhg <vijithhgedu@gmail.com>
2024-09-23 20:34:25 +0530

    db connected

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const cors = require('cors')
 const employeeRoutes = require('./routes/employeeRoutes')
 
@@ -8,6 +9,13 @@ app.use(cors())
 
 app.use('/employee',employeeRoutes)
 
+
+//mongoDB connection
+mongoose.connect('mongodb://127.0.0.1:27017/mvc').then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.listen(8000,()=>{
     console.log('server running')
